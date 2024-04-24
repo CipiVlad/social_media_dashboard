a0d9989b93a7fcc3bbe8585d5be0cbcb5997bb6d
CipiVlad <supercoder.cipi@gmail.com>
2024-04-24 23:58:37 +0200

    add bgCard with gray color to lightmode

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
     dark: {
         background: 'hsl(230, 17%, 14%)',
-        backgroundTop: 'hsl(232, 19%, 15%)',
+        backgroundTop: 'hsl(220, 19%, 15%)',
         backgroundCard: 'hsl(228, 28%, 20%)',
         backgroundToggle: 'linear-gradient(hsl(210, 78%, 56%), hsl(146, 68%, 55%))',
         text: 'hsl(0, 0%, 100%)',
@@ -10,6 +10,7 @@ export const theme = {
 
     light: {
         background: 'hsl(0, 0%, 100%)',
+        backgroundTop: 'hsl(225, 100%, 98%)',
         backgroundCard: 'hsl(227, 47%, 96%)',
         backgroundToggle: 'linear-gradient(hsl(210, 78%, 56%), hsl(146, 68%, 55%))',
         text: 'hsl(230, 17%, 14%)',
